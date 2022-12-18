cea7e1c796d103b52773adaecb3b44564c15fc2c
Mathiasko <laco.matus@gmail.com>
2022-12-18 22:10:05 +0100

    0.1

---
+++ src/api/en-page/controllers/en-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * en-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::en-page.en-page');
---
+++ src/api/en-page/services/en-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * en-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::en-page.en-page');
